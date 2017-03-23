38d4a459a8451cdba4f4db065a1fbda331b8d4a3
Padraig Gleeson <p.gleeson@gmail.com>
2017-03-23 18:32:22 +0000

    More work on net gen

--- NeuroML2/network/create_new_lems_file.py
+++ NeuroML2/network/create_new_lems_file.py
@@ -4,7 +4,6 @@ Example to create some new LEMS files for running NML2 models
 
 '''
 
-from pyneuroml.lems import LEMSSimulation
 from pyneuroml.lems import generate_lems_file_for_neuroml
 import os
 
@@ -16,15 +15,18 @@ if __name__ == '__main__':
     ###  Create the LEMS file with helper method
     
     
-    net = 'MiniScale_HippocampalNet'
+    net = 'HippocampalNet'
+    net = 'LargeScale_HippocampalNet'
     
     sim_id = 'Sim_%s'%net
     neuroml_file = "%s.net.nml"%net
     target = net
-    duration=1000
+    duration=100
     dt = 0.025
     lems_file_name = 'LEMS_%s.xml'%sim_id
     target_dir = "."
+    
+    pops_with_morphlogies = ["pop_ngf","pop_bistratified","pop_sca","pop_olm","pop_poolosyn","pop_pvbasket","pop_cck","pop_axoaxonic","pop_ivy"]
     
     generate_lems_file_for_neuroml(sim_id, 
                                    neuroml_file, 
@@ -34,14 +36,12 @@ if __name__ == '__main__':
                                    lems_file_name,
                                    target_dir,
                                    include_extra_files = [],
-                                   gen_plots_for_all_v = True,
+                                   gen_plots_for_all_v = False,
                                    plot_all_segments = False,
-                                   gen_plots_for_quantities = {},   #  Dict with displays vs lists of quantity paths
-                                   gen_plots_for_only_populations = [],   #  List of populations, all pops if = []
-                                   gen_saves_for_all_v = True,
+                                   gen_plots_for_only_populations = pops_with_morphlogies,   #  List of populations, all pops if = []
+                                   gen_saves_for_all_v = False,
                                    save_all_segments = False,
-                                   gen_saves_for_only_populations = [],  #  List of populations, all pops if = []
+                                   gen_saves_for_only_populations = pops_with_morphlogies,  #  List of populations, all pops if = []
                                    gen_saves_for_quantities = {},   #  Dict with file names vs lists of quantity paths
-                                   gen_spike_saves_for_all_somas = True,
                                    copy_neuroml = True,
                                    seed=None)
